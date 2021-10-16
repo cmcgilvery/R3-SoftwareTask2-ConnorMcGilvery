bd2d5e3253e92c9fbd2e8ec66a37c243397d3099
cmcgilvery <91976254+cmcgilvery@users.noreply.github.com>
2021-10-16 13:29:54 -0400

    Add files via upload

---
+++ client.py
@@ -0,0 +1,41 @@
+import socket
+import keyboard
+import time
+
+PORT = 5030 #port that the server runs on
+HOST = socket.gethostbyname(socket.gethostname()) #get local ip address
+speed = 10
+direction = ""
+validdirection = False
+endprogram = False
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  #create client socket
+client.connect((HOST, PORT)) #connect client socket to port and host ip address
+
+def send(input):
+    data = input.encode('utf-8') #encodes data into byte form
+    client.send(data) #sends data to server
+
+while endprogram == False:
+    print("Enter a digit from 0-5 to choose the speed or l,r,b,w to go left, right, backward or forward respectivly or x to exit")
+
+    direction = keyboard.read_key()  # reads key into direction variable
+    if (direction == "l") | (direction =="r") | (direction =="b") | (direction =="w") :  # determines if direction input is valid
+        validdirection = True
+    if keyboard.read_key() == "x":  # if x then program ends and a disconnect message is sent to the server
+        send("disconnect")
+        endprogram = True
+    if validdirection:  # if direction input is valid, then send it to the server
+        send(direction)
+    else:
+        try:
+            speed = int(direction) #converts key pressed to int then writes it to variable speed
+            if speed <6:  # send speed to server if valid
+                send(str(speed))
+        except:
+            print ('')
+
+    validdirection = False
+
+
+
---
+++ server.py
@@ -0,0 +1,50 @@
+import socket
+
+PORT = 5030 #port that the server runs on
+SERVER = socket.gethostbyname(socket.gethostname()) #get local ip address
+speed = 0
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  #create server socket
+server.bind((SERVER, PORT)) #connect server socket to port and host ip address
+m1 = '' #motor states
+m2 = ''
+m3 = ''
+m4 = ''
+server.listen()
+conn, addr = server.accept()  # recieves data from client
+endprogram = False
+while endprogram == False:
+    data = conn.recv(16).decode('utf-8') #decodes data from byte form to string
+
+    if data == "disconnect": #ends program if a disconnect message is recieved
+        endprogram = True
+    if data == "l":
+        m1 = 'r'
+        m2 = 'r'
+        m3 = 'f'
+        m4 = 'f'
+    if data == "r":
+        m1 = 'f'
+        m2 = 'f'
+        m3 = 'r'
+        m4 = 'r'
+    if data == "w":
+        m1 = 'f'
+        m2 = 'f'
+        m3 = 'f'
+        m4 = 'f'
+    if data == "b":
+        m1 = 'r'
+        m2 = 'r'
+        m3 = 'r'
+        m4 = 'r'
+    elif (endprogram == False) & (data != 'r') & (data !='l') & (data !='w') & (data !='b'):
+        speed = 51*int(data) #scales speed input of 0-5 to 0-255 which represents pwm value
+
+    print(f"[{m1}{speed}][{m2}{speed}][{m3}{speed}][{m4}{speed}]") #display motor configuration determined by speed and direction input
+conn.close()
+
+
+
+
+
